9ea13e5637fbf87ef5ea63bb0e0e064c00ed48f1
André Tobias <andre_tobias6621@outlook.com>
2025-01-30 21:21:48 -0300

    Criação do projeto MVC

---
+++ Sistema.De.Notas/Sistema.De.Notas/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SistemaDeNotas.Data
+{
+    //Banco de dados
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        // Tabela nota aluno 
+        public DbSet<NotaAluno> NotasAlunos { get; set; } = default!;
+    }
+
+    public class NotaAluno
+    {
+        public int Id { get; set; }
+        public string NomeAluno { get; set; } = string.Empty;
+        public double Dic1 { get; set; }
+        public double Dic2 { get; set; }
+        public double Dic3 { get; set; }
+        public double Dic4 { get; set; }
+        public double Dic5 { get; set; }
+        public double PresencaPercentual { get; set; }
+
+        public double CalcularMedia() => (Dic1 + Dic2 + Dic3 + Dic4 + Dic5) / 5;
+    }
+}
---
+++ Sistema.De.Notas/Sistema.De.Notas/Controllers/NotaAlunoController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SistemaDeNotas.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaDeNotas.Controllers
+{
+    public class NotaAlunoController : Controller
+  
+    {
+        // Banco de dados 
+        private readonly AppDbContext _context;
+
+        public NotaAlunoController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // Consulta as notas no banco
+            var notasAlunos = await _context.NotasAlunos.ToListAsync();
+            return View(notasAlunos);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(NotaAluno notaAluno)
+        {
+            if (ModelState.IsValid)
+            {
+                //Salva uma nova nota do aluno 
+                _context.NotasAlunos.Add(notaAluno);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(notaAluno);
+        }
+
+        public async Task<IActionResult> CalcularMedias()
+
+        {
+
+            var notasAlunos = await _context.NotasAlunos.ToListAsync();
+
+            if (!notasAlunos.Any())
+
+            {
+
+                TempData["Mensagem"] = "Nenhum aluno cadastrado.";
+
+                return RedirectToAction(nameof(Index));
+
+            }
+
+           
+
+            double mediaTurma = notasAlunos.Average(n => n.CalcularMedia());
+
+            
+
+            TempData["MediaTurma"] = mediaTurma.ToString("F2"); 
+
+            return RedirectToAction(nameof(Index));
+
+        }
+
+
+    }
+}
